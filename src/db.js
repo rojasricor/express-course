6189ffddf14beb9cd9418eacb762076a2d3d8607
rojasricor <rojasricor@gmail.com>
2023-04-20 22:04:08 -0500

    ended

--- src/db.js
+++ src/db.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2/promise");
 
 async function connectDB() {
   const connection = await mysql.createConnection({
-    host: "aws.connect.psdb.cloud",
-    user: "1nukcn2396n00k3g7caa",
-    password: "pscale_pw_Tq5CU1XGqLuu3ZkzaTiA2t8LCyvHlunCkmrjAn209bh",
+    host: "127.0.0.1",
+    user: "admin",
+    password: "admin",
     database: "itfip",
     ssl: {
       rejectUnauthorized: false,
